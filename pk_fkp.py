0cd9b01a5d2033777f9dda475a73477bb8815d08
arthurmloureiro <arthurmloureiro@gmail.com>
2014-07-31 18:00:48 -0300

    Using Raul's selection function, generating maps to Secco

--- pk_fkp.py
+++ pk_fkp.py
@@ -56,7 +56,7 @@ integrando=sinkr*termo2
 
 corr_ln=np.power(2.0*np.pi*np.pi,-1.0)*np.sum(integrando,axis=0)       		     # uses the trace in the r axis to make the integral
 corr_g = np.log(1.+corr_ln) 	
-						     # Gaussian Correl. Func.
+						                                     # Gaussian Correl. Func.
 ######################################
 # Finding the gaussian power spectrum
 ######################################
@@ -106,16 +106,17 @@ def delta_x_ln(d_,sigma_):
 # Selection funtion
 ####################
 def phi_selec(r_):
-	#f1 = np.exp(-(r_*r_)/(2.*al*al))
-	#norm = np.sqrt(2.*np.pi)*al
-	#return f1/norm
-	a1 = np.power((r_ - 350.),2.)/((950.)**2.)
-	a2 = np.power((r_-400.), 2.)/((930.)**2.)
-	return (2.7)*np.exp(-a1-a2)
+	a1 = 0.00141161
+	a2 = 2.28162E-6
+	a3 = 1.01031E-9
+	a4 = 4.89549E-14
+	norm = 0.834923
+	return norm*np.exp(-a1*r_ + a2*np.power(r_,2.)-a3*np.power(r_,3.)+a4*np.power(r_,4.))
 phi_selec_vec = np.vectorize(phi_selec)
 n_bar = phi_selec_vec(grid.grid_r*cell_size)*n_bar0
 
 N_bar_bins=np.zeros(((num_bins,num_bins,num_bins)))
+nr_bins=np.zeros(((num_bins,num_bins,num_bins)))
 
 ################################################################
 # FFT Loops for Gaussian and Gaussian + Poissonian Realizations
@@ -123,6 +124,9 @@ N_bar_bins=np.zeros(((num_bins,num_bins,num_bins)))
 k_bar = np.arange(0,num_bins,1)*(np.max(grid.grid_k)/num_bins)
 inicial = clock()
 pl.figure()
+media_1 = np.zeros((num_realiz, num_bins))
+media_1n = np.zeros((num_realiz, num_bins))
+file = open('supergrid2.dat','w')
 if realiz_type == 1:
 	print "Doing both Gaussian + Poissonian realizations... \n"
 	for m in range(num_realiz):
@@ -144,24 +148,38 @@ if realiz_type == 1:
 		#######################
 		N_r = np.random.poisson(n_bar*(1.+delta_xr)*(cell_size**3.))			     # This is the final galaxy Map
 		N_i = np.random.poisson(n_bar0*(1.+delta_xi)*(cell_size**3.))
-		n_bar0_new = np.mean(N_r)
+		#n_bar0_new = np.mean(N_r)
 		#############################
 		# Spliting the Map into bins
 		#############################
 		Nx = np.split(N_r,num_bins)
+		nrx = np.split(n_bar,num_bins)
 		for i in range(len(Nx)):
 			Nxy = np.split(Nx[i], num_bins, axis=1)
+			nrxy = np.split(nrx[i], num_bins, axis=1)
 			for j in range(len(Nxy)):
 				Nxyz = np.split(Nxy[j], num_bins, axis=2)
+				nrxyz = np.split(nrxy[j], num_bins, axis=2)
 				for l in range(len(Nxyz)):
 					N_bar_bins[i][j][l] = np.mean(Nxyz[l])/(cell_size**3.)
+					nr_bins[i][j][l] = np.mean(nrxyz[l])
+		media_1[m,:] = N_bar_bins[0][0] ##################################################################
+		media_1n[m,:] = nr_bins[0][0]
+		###############################################################
+		# this loop saves the galaxy map so Lucas' program can read it 
+		###############################################################
+		for i in range(n_x):
+			for j in range(n_y):
+				for l in range(n_z):
+					file.write(",%d"%int(N_r[i,j,l]))
+		
 		#pol_coef = np.polyfit(grid_bins.grid_r[0][0], np.log(N_bar_bins[0][0]),4 )
 		#fit = np.poly1d(pol_coef)
 		#pol_coef2 = np.polyfit(grid_bins.grid_r[0][0], np.log(N_bar_bins[0][0]),3 )
 		#fit2 = np.poly1d(pol_coef2)
 		#pl.plot(fit(grid_bins.grid_r[0][0]),color="r")
 		#pl.plot(fit2(grid_bins.grid_r[0][0]),color="k")
-		pl.plot(np.log(N_bar_bins[0][0]), linewidth=1, color="k")
+		#pl.plot(np.log(N_bar_bins[0][0]), linewidth=1, color="k")
 		##########################################
 		#$%%$ AQUI SEGUE O CÓDIGO PARA O FKP $%%$#
 		##########################################
@@ -186,22 +204,29 @@ elif realiz_type == 2:
 		#######################
 		#poissonian realization
 		#######################
-		N_r = np.random.poisson(n_bar*(1.+delta_xr)*(cell_size**3.))			     # This is the final galaxy Map
+		N_r = np.random.poisson(n_bar*(1.+delta_xr)*(cell_size**3.))     # This is the final galaxy Map
 		N_i = np.random.poisson(n_bar0*(1.+delta_xi)*(cell_size**3.))
 		n_bar0_new = np.mean(N_r)
 		#############################
 		# Spliting the Map into bins
 		#############################
 		Nx = np.split(N_r,num_bins)
+		nrx = np.split(n_bar,num_bins)
 		for i in range(len(Nx)):
 			Nxy = np.split(Nx[i], num_bins, axis=1)
+			nrxy = np.split(nrx[i], num_bins, axis=1)
 			for j in range(len(Nxy)):
 				Nxyz = np.split(Nxy[j], num_bins, axis=2)
+				nrxyz = np.split(nrxy[j], num_bins, axis=2)
 				for l in range(len(Nxyz)):
-					N_bar_bins[i][j][l] = np.mean(Nxyz[l])
-		pol_coef = np.polyfit(grid_bins.grid_r[0][0], np.log(N_bar_bins[0][0]),4 )
-		fit = np.poly1d(pol_coef)
-		pl.plot(fit(grid_bins.grid_r[0][0]), color="r")
+					N_bar_bins[i][j][l] = np.mean(Nxyz[l])/(cell_size**3.)
+					nr_bins[i][j][l] = np.mean(nrxyz[l])
+		media_1[m,:] = N_bar_bins[0][0] ##################################################################
+		media_1n[m,:] = nr_bins[0][0] ##################################################################
+	#	media_1[m,:] = N_r[30][30] ##################################################################
+		#pol_coef = np.polyfit(grid_bins.grid_r[0][0], np.log(N_bar_bins[0][0]),4 )
+		#fit = np.poly1d(pol_coef)
+		#pl.plot(fit(grid_bins.grid_r[0][0]), color="r")
 		##########################################
 		#$%%$ AQUI SEGUE O CÓDIGO PARA O FKP $%%$#
 		##########################################
@@ -209,17 +234,26 @@ elif realiz_type == 2:
 else:
 	print "Error, invalid option for realization's type \n"
 	sys.exit(-1)
+file.close()
+media_2 = np.zeros(num_bins)
+media_2n = np.zeros(num_bins)
+for i in range(num_bins):
+	media_2[i] = np.mean(media_1[:,i])
+	media_2n[i] = np.mean(media_1n[:,i])
+	
 
 #pl.plot(np.log(N_bar_bins[0][0]), linewidth=2)
-pl.plot(np.log(phi_selec((grid_bins.grid_r[0][0]*5.*cell_size))*n_bar0),linewidth=2.5)
+#pl.plot(np.log(phi_selec((grid_bins.grid_r*(float(n_x)/num_bins)*cell_size))[0][0]*n_bar0),linewidth=2.5)
+pl.plot(np.arange(num_bins+1)[1:]*(cell_size*3),np.log(media_2))
+pl.plot(np.arange(num_bins+1)[1:]*(cell_size*3),np.log(media_2n), linewidth=2.0)
 final = clock()
 print "time = " + str(final - inicial)		
 pl.figure()
 pl.imshow(N_r[0], cmap=cm.jet)
+#pl.figure()
+#pl.imshow(N_i[0], cmap=cm.jet)
 pl.figure()
-pl.imshow(N_i[0], cmap=cm.jet)
-pl.figure()
-pl.imshow(N_bar_bins[1], cmap=cm.jet, interpolation="nearest")
+pl.imshow(N_bar_bins[0], cmap=cm.jet, interpolation="nearest")
 pl.show()
 
 
